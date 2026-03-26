fbcaeb8a54053613be53204bdb609d218f90e495
Sadorra <rodiawg@gmail.com>
2026-03-26 22:08:29 +0300

    refactor: Secure version of router.

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,15 +1,25 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { createSupabaseServerClient } from "@/src/lib/supabaseServer";
 
 export async function POST(req: Request) {
   try {
+    // Create supabase server client
+    const supabase = createSupabaseServerClient();
+
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Unauthorized" },
+        { status: 401 }
+      );
+    }
 
     const body = await req.json();
     const items = body.items;
-
-    console.log("Items received:", items);
-
-    console.log("Stripe key:", process.env.STRIPE_SECRET_KEY);
 
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
       apiVersion: "2026-02-25.clover",
@@ -23,14 +33,11 @@ export async function POST(req: Request) {
       line_items: items.map((item: any) => ({
         price_data: {
           currency: "usd",
-
           product_data: {
             name: item.name,
           },
-
           unit_amount: Math.round(item.price * 100),
         },
-
         quantity: item.quantity,
       })),
 
@@ -38,14 +45,9 @@ export async function POST(req: Request) {
       cancel_url: "http://localhost:3000/cart",
     });
 
-    console.log("Stripe session created:", session.id);
-
-    return NextResponse.json({
-      url: session.url,
-    });
+    return NextResponse.json({ url: session.url });
 
   } catch (error) {
-
     console.error("CHECKOUT ERROR:", error);
 
     return NextResponse.json(
